5f5d9d8957cdc0b80d22bb3418026fd91cff17c5
Patrik Barkman <barkm@kth.se>
2016-10-05 12:02:00 +0200

    Started on new Maze class

--- Code/src/Maze_old.java
+++ Code/src/Maze_old.java
@@ -2,43 +2,31 @@ import java.util.LinkedList;
 import java.util.Collections;
 
 public class Maze {
-
-    // Wall probability
-    private static double WALL_PROB = 0.7;
-
-    // Store cells
-	private Cell[][] mazeData;
+	private int[][] mazeData;
 	
 	public Maze(){
 	}
 	
-	public Maze(Cell[][] mazeData){
+	public Maze(int[][] mazeData){
 		this.setMaze(mazeData);
 	}
 
-	public Cell[][] getMaze() {
+	public int[][] getMaze() {
 		return mazeData;
 	}
 
-	public void setMaze(Cell[][] mazeData) {
+	public void setMaze(int[][] mazeData) {
 		this.mazeData = mazeData;
 	}
 	
 	public void randomMaze(int width,int height){
-		Cell[][] newMazeData = new Cell[width][height];
-
-		for (int x = 0; x < width; x++){
-			for (int y = 0; y < height; y++){
-
-				if (Math.random() > WALL_PROB){ // Cell is wall
-					newMazeData[x][y] = new Cell(true);
-				}
-                else{                           // Cell is empty
-                    newMazeData[x][y]= new Cell(false);
-                }
-            }
-        }
-
+		int[][] newMazeData = new int[width][height];
+		for (int x = 0;x<width;x++){
+			for (int y = 0;y<height;y++){
+				newMazeData[x][y]=0;
+				if (Math.random()>0.7){
+					newMazeData[x][y]=1;
+				}}}
 		setMaze(newMazeData);
 	}
 	
@@ -49,9 +37,7 @@ public class Maze {
 	public int getHeight(){
 		return mazeData[0].length;
 	}
-
-
-    /*
+	
 	public void primsMaze(int width,int height){
 		int[][] newMazeData = new int[width][height];
 		for (int x = 0;x<width;x++){
@@ -112,19 +98,24 @@ public class Maze {
 		newMazeData[xs][ys] = 10;
 		setMaze(newMazeData);
 	}
-	*/
-
-
+	
 	public void easyfy(double chance){
-		for (int x = 0; x < getWidth(); x++){
-			for (int y = 0; y < getHeight(); y++){
-				if (mazeData[x][y] == 1){
-					if(Math.random() > chance){
-						mazeData[x][y] = new Cell(false);
-                    }
-                }
-            }
-        }
+		for (int x = 0;x<getWidth();x++){
+			for (int y = 0;y<getHeight();y++){
+				if (mazeData[x][y]==1){
+					if(Math.random()>chance){
+						mazeData[x][y]=0;
+						}}}}
+	}
+
+	public static void main(String[] args){
+		int[][] mazeData = {{0,1,0,1,0},{0,1,0,1,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,1}};
+		Maze maze = new Maze();
+		System.out.println("here");
+		maze.primsMaze(100, 100);
+		maze.easyfy(0.9);
+		
+		new MazeGui(maze);
 	}
 
 }
