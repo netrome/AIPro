37e72932c12d59ea61b4514e90790fd54842256d
Marten <marten3@kth.se>
2016-10-08 14:00:50 +0200

    No major changes...

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         int[] startPos = maze.getFreePos();
         //System.out.println(maze.getCell(startPos[0], startPos[1]).isWall());
         
-        int numberOfAgents = 4;
+        int numberOfAgents = 8;
         Agent[] agents = new Agent[numberOfAgents];
         for (int i=0;i<numberOfAgents;i++)agents[i]=new Agent(startPos[0],startPos[1],maze);
         State state = new State(agents,maze);
@@ -29,7 +29,7 @@ public class Main {
         	agents=state.agents;
         	gui.changeMaze(state.maze);
         	try {
-        	    Thread.sleep(100);
+        	    Thread.sleep(40);
         	} catch(InterruptedException ex) {
         	    Thread.currentThread().interrupt();
         	}
