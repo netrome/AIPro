e9320af1d06bf41e2cdf32584bcde8017ae23bac
Patrik Barkman <barkm@kth.se>
2016-10-09 15:22:26 +0200

    Implemented AStar but it is very slow atm

--- Code/src/AStar.java
+++ Code/src/AStar.java
@@ -4,42 +4,50 @@ public class AStar {
 
 
     /**
-     * A* search based on a heuristic h(n)
-     *
-     * f(n) = h(n) + g(n)
+     * A* search based on a heuristic
      *
      * where f(n) is the cost from the start to n and g(n) is the
      * cost from n to the goal
      *
      * @param start: the start state
-     * @param goal: the goal state
-     * @param f: the heuristic
+     * @param h: the heuristic
      * @return: The optimal path as a list of states (from start to goal). Returns empty list if no path was found
      */
-    public static List<State> search(State start, State goal, Successor s, Heuristic f){
+    public static List<State> search(State start, Successor s, Heuristic h){
+
 
         // All visited states
-        Set<State> visited = new HashSet<>();
+        Set<String> visited = new HashSet<>();
 
         // Best predecessor state
         Map<State, State> predecessor = new HashMap<>();
 
-        // Cost from the start to the goal
-        Map<State, Double> costFromGoal = new HashMap<>();
+        // Cost from the start to a state
+        Map<String, Double> costFromStart = new HashMap<>();
+
+        // Total cost for a state
+        Map<State, Double> cost = new HashMap<>();
 
         // States to be evaluated
-        PriorityQueue<State> heap = new PriorityQueue<>(0, new AStar.CostComparator(costFromGoal, f));
+        PriorityQueue<State> heap = new PriorityQueue<>(1, new AStar.CostComparator(cost));
+
+        // Add start to heap
+        heap.add(start);
+
+        // Cost to start is 0
+        costFromStart.put(start.toString(), new Double(0));
 
         while(!heap.isEmpty()){
 
             // Get state with lowest cost value
             State current = heap.poll();
+            String currentString = current.toString();
 
             // Add to visited
-            visited.add(current);
+            visited.add(current.toString());
 
             // Check if goal
-            if (current == goal){
+            if (s.isGoal(current)){
                 return reconstructPath(current, predecessor);
             }
 
@@ -48,39 +56,44 @@ public class AStar {
 
             for (State neighbour : nextPossibleStates){
 
+                String neighbourString = neighbour.toString();
+
                 // Don't check already visited
-                if(visited.contains(n)){
+                if(visited.contains(neighbourString)){
                     continue;
                 }
 
                 // Calculate cost from goal
-                double tentativeCostFromGoal = costFromGoal.get(current) + f.distance(current, neighbour);
+                double tentativeCostFromGoal = costFromStart.get(currentString) + h.distance(current, neighbour);
 
-                // Add to heap if not seen before
-                if (!heap.contains(neighbour)){
-                    heap.add(neighbour);
-                }
-                // If already seen and more costly than before
-                else if(tentativeCostFromGoal >= costFromGoal.get(neighbour)){
+
+                // If already seen ad more costly than before
+                if(costFromStart.containsKey(neighbourString) && tentativeCostFromGoal >= costFromStart.get(neighbourString)){
                     continue;
                 }
 
-                // Else this is the best path until now
+                // Else this is the best or only path until now
 
                 // Current was the predecessor
                 predecessor.put(neighbour, current);
 
                 // Store the cost from goal
-                costFromGoal.put(neighbour, tentativeCostFromGoal);
-            }
+                costFromStart.put(neighbourString, tentativeCostFromGoal);
 
-            // If no path was found return empty list
-            return new ArrayList<>();
+                // Store total cost
+                cost.put(neighbour, tentativeCostFromGoal + h.costToGoal(neighbour));
+
+                // Add to heap if not seen before
+                if (!heap.contains(neighbour)){
+                    heap.add(neighbour);
+                }
+
+            }
 
         }
 
-
-        return null;
+        // If no path was found return empty list
+        return new ArrayList<>();
     }
 
     private static List<State> reconstructPath(State lastState, Map<State, State> predecessor){
@@ -89,7 +102,7 @@ public class AStar {
 
         State s = lastState;
 
-        while (!predecessor.containsKey(s)){
+        while (predecessor.containsKey(s)){
             path.add(0, s);
             s = predecessor.get(s);
         }
@@ -108,25 +121,21 @@ public class AStar {
 
     static private class CostComparator implements Comparator<State>{
 
-        Heuristic f;
-        Map<State, Double> costFromStart;
+        Map<State, Double> cost;
 
-        private CostComparator(Map<State, Double> costFromStart, Heuristic f){
-            this.f = f;
-            this.costFromStart = costFromStart;
+        private CostComparator(Map<State, Double> cost){
+            this.cost = cost;
         }
 
         public int compare(State s1, State s2) {
-            double cost1 = costFromStart.get(s1) + f.costToGoal(s1);
-            double cost2 = costFromStart.get(s2) + f.costToGoal(s2);
-
-            return (int) (cost1 - cost2);
+            return (int) (cost.get(s1) - cost.get(s2));
         }
 
     }
 
     public interface Successor{
         public List<State> successors(State s);
+        public boolean isGoal(State s);
     }
 
 
--- Code/src/GlobalPlayer.java
+++ Code/src/GlobalPlayer.java
@@ -6,18 +6,55 @@ import java.util.List;
  */
 public class GlobalPlayer implements Player {
 
+    private State goal;
+
+
     public GlobalPlayer(){
 
     }
 
     @Override
-    public State play(State gameState) {
-        for (Agent agent : gameState.agents){
-            List<int []> moves = agent.getPossibleMoves();
-            Collections.shuffle(moves);
-            agent.move(moves.get(0)[0], moves.get(0)[1]);
+    public State play(State state) {
+
+
+        List<State> path = AStar.search(state, new GlobalSuccessor(), new GlobalHeuristic());
+
+        if(path.size() == 0){
+            System.err.println("No path!");
+            return state;
+        }
+        else if(path.size() == 1){
+            System.err.println("Reached goal!");
+            return state;
         }
 
-        return gameState;
+        return path.get(1);
     }
+
+
+    private class GlobalSuccessor implements AStar.Successor{
+
+        public List<State> successors(State s){
+            return s.findPossibleMoves();
+        }
+
+        public boolean isGoal(State s){
+            return s.isEOG();
+        }
+
+    }
+
+    private class GlobalHeuristic implements AStar.Heuristic{
+
+        public double distance(State s1, State s2){
+            return 1 + (s1.maze.getExplored() - s2.maze.getExplored());
+        }
+
+
+        public double costToGoal(State s){
+            return 0;
+        }
+
+    }
+
 }
