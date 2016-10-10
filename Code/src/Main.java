c859973bf7e59b54887bf2dfa13574b46d4edf61
Marten <marten3@kth.se>
2016-10-10 09:44:55 +0200

    Nothing really, just running the program with different parameters

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -11,12 +11,12 @@ public class Main {
     public static void main(String[] args) {
         Maze maze = new Maze();
         maze.primsMaze(50, 50);
-        maze.easyfy(1);
-        Player player = new AntsPlayer();
+        maze.easyfy(0.2);
+        Player player = new MDFSPlayer();
         int[] startPos = maze.getFreePos();
         //System.out.println(maze.getCell(startPos[0], startPos[1]).isWall());
         
-        int numberOfAgents = 8;
+        int numberOfAgents = 18;
         Agent[] agents = new Agent[numberOfAgents];
         for (int i=0;i<numberOfAgents;i++)agents[i]=new Agent(startPos[0],startPos[1],maze);
         State state = new State(agents,maze);
